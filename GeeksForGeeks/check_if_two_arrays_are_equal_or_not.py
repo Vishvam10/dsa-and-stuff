6bc84e339808c6d3bef5b8eb684e22e1da27a312
Vishvam10 <s.vishvam1025@gmail.com>
2022-08-29 20:56:23 +0530

    Added GeeksForGeeks Problems and Renaming Script

---
+++ GeeksForGeeks/check_if_two_arrays_are_equal_or_not.py
@@ -0,0 +1,22 @@
+class Solution:
+    #Function to check if two arrays are equal or not.
+    def check(self, a, b, n):
+        #return: True or False
+        mp = {}
+        for x in a :
+            if x in mp.keys() :
+                mp[x] += 1
+            else :
+                mp[x] = 1
+        
+        for x in b :
+            if x not in mp.keys() :
+                return False
+            else :
+                mp[x] -= 1
+                
+        for v in mp.values() :
+            if(v < 0) :
+                return False
+                
+        return True
