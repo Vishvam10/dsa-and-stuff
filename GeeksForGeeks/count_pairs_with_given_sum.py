9d96bf66a8e8060015e72d562c002412c52c922d
Vishvam10 <s.vishvam1025@gmail.com>
2022-08-31 21:01:24 +0530

    Added count_pairs_with_given_sum

---
+++ GeeksForGeeks/count_pairs_with_given_sum.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def getPairsCount(self, arr, n, k):
+        mp = {}
+        count = 0
+        for i in range(n) :
+            if arr[i] not in mp.keys() :
+                mp[arr[i]] = 1
+            else :
+                mp[arr[i]] += 1
+        for i in range(n) :
+            if((k - arr[i]) in mp) :
+                mp[arr[i]] -= 1
+                count += mp[k - arr[i]]
+        
+        return count 
