92f5401e627fe7e6ed2285356c2391a29f7ae794
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-02 23:03:01 +0530

    Added 1630, 2221

---
+++ LeetCode/1630.py
@@ -0,0 +1,13 @@
+class Solution:        
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        ans = []
+        for i in range(len(l)):
+            arr = nums[l[i]:r[i]+1]
+            temp = True
+            arr.sort()
+            for i in range(len(arr)-1):
+                if (arr[i+1] - arr[i] != arr[1] - arr[0]):
+                    temp = False
+                    break
+            ans.append(temp)
+        return ans
