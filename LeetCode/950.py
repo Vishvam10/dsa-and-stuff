9137f9009603c0b505c93c4dc2940c0ab6c8e535
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-06 21:57:03 +0530

    Added 950

---
+++ LeetCode/950.py
@@ -0,0 +1,7 @@
+class Solution:
+    def deckRevealedIncreasing(self, arr: List[int]) -> List[int]:
+        ans = []
+        for x in sorted(arr)[::-1]:
+            ans = [x] + ans[-1:] + ans[:-1]
+        return ans
+            
