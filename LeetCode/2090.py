45823f416c1264fabaf872f570990eb85c5aaafe
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-07 23:55:43 +0530

    Added 2090

---
+++ LeetCode/2090.py
@@ -0,0 +1,12 @@
+class Solution:
+    # Use prefix sums
+    def getAverages(self, arr: List[int], k: int) -> List[int]:
+        ans = [-1] * len(arr)
+        left, s, dia = 0, 0, 2*k+1
+        for right in range(len(arr)):
+          s += arr[right]
+          if (right - left + 1 >= dia):
+            ans[left + k] = s // dia
+            s -= arr[left]
+            left += 1
+        return ans
