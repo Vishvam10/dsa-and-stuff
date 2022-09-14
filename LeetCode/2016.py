520e55f89fdbd7b57e8feb863efda37657ad63e6
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-14 23:17:05 +0530

    Added 2016

---
+++ LeetCode/2016.py
@@ -0,0 +1,13 @@
+class Solution:
+    # Initialize mn = 0 and start from i = 1
+    # This guarantees the j > i condition
+    def maximumDifference(self, arr: List[int]) -> int:
+        mn, ans = arr[0], -1
+        for i in range(1, len(arr)) :
+            ans = max(ans, arr[i] - mn)
+            mn = min(mn, arr[i])
+        if(ans == 0) :
+            return -1
+        return ans
+            
+        
