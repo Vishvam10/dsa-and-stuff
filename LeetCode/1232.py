497b7cd98333d83b59488012f0170ef788734fac
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-18 16:31:52 +0530

    Added 1232, 593

---
+++ LeetCode/1232.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkStraightLine(self, arr: List[List[int]]) -> bool:
+        (x1, y1), (x2, y2) = arr[:2]
+        for i in range(2, len(arr)):
+            (x, y) = arr[i]
+            if((y2 - y1) * (x1 - x) != (y1 - y) * (x2 - x1)):
+                return False
+        return True		
