c9cf5b8c1cc5ab9599bbdefc13a34ca19d2b24bd
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-21 22:42:27 +0530

    Added 150, 98

---
+++ LeetCode/150.py
@@ -0,0 +1,22 @@
+class Solution:
+    # IMPORTANT NOTE :  "//" is not equal to 
+    # "int(float(a)/b)" i.e. truncate to zero
+    
+    # We don't check for stack underflow because the 
+    # question says that the solution always exists
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for t in tokens:
+            if t not in "+-*/":
+                stack.append(int(t))
+            else:
+                r, l = stack.pop(), stack.pop()
+                if t == "+":
+                    stack.append(l+r)
+                elif t == "-":
+                    stack.append(l-r)
+                elif t == "*":
+                    stack.append(l*r)
+                else:
+                    stack.append(int(float(l)/r))
+        return stack.pop()
---
+++ LeetCode/98.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Create a helper function for recursion
+    
+    # Use 2 global variables low and high to keep track of
+    # the minimum and maximum value upto that node.  
+    
+    # The low and high update is simple : 
+    # FOR LEFT SUBTREE : low = low ( no change ), high = root.val
+    # FOR RIGHT SUBTREE : low = root.val, high = high ( no change )
+    
+    
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def traverse(root, low, high) :
+            if not root :
+                return True
+            elif not low < root.val < high :
+                return False
+            else :
+                return traverse(root.left, low, root.val) and traverse(root.right, root.val, high)
+        return traverse(root, low=float("-inf"), high=float("inf"))
+            
