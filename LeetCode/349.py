40f7704503c3d5918a641e234aa21511710e9bb9
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-22 17:34:12 +0530

    Added 349

---
+++ LeetCode/349.py
@@ -0,0 +1,7 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(nums1)) :
+            if(nums1[i] in nums2) :
+                ans.append(nums1[i])
+        return set(ans)
