9e55c51d1a1bda83d0f0e8ec41015c1e25044dd1
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-24 14:16:05 +0530

    Added 1480, 704, 724

---
+++ LeetCode/1480.py
@@ -0,0 +1,7 @@
+class Solution:
+    def runningSum(self, arr: List[int]) -> List[int]:
+        pre = [None]*len(arr)
+        pre[0] = arr[0]
+        for i in range(1, len(arr)) :
+            pre[i] = pre[i-1] + arr[i]
+        return pre
---
+++ LeetCode/704.py
@@ -0,0 +1,12 @@
+class Solution:
+    def search(self, arr: List[int], target: int) -> int:
+        low, high = 0, len(arr) - 1
+        while(low <= high) :
+            mid = low + (high - low) // 2
+            if(arr[mid] == target) :
+                return mid
+            elif(target > arr[mid]) :
+                low = mid + 1
+            else :
+                high = mid - 1
+        return -1
