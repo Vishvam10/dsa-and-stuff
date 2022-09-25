4f5a44e9ee3fa4815ddf710f06dfa557dcc22f41
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-25 23:54:57 +0530

    Updated rename script

--- Scripts/rename_files.py
+++ Scripts/rename_files.py
@@ -1,12 +1,29 @@
 import os
 
-NOT_ALLOWED = [".git", "README.md", "Scripts"] 
+NOT_ALLOWED = [".git", "README.md", "Scripts", "Topics"] 
 
-for directory in os.listdir(os.path.abspath("../")) :
-    if(directory not in NOT_ALLOWED) :
-        abs_path = "../{}".format(directory)
-        for file in os.listdir(abs_path):
-            new_name = "{}.py".format(file.replace(
-                " ", "_").replace("-", "_").replace(",", "").lower().split(".")[0])
+def rename_files(folder, extension) :
+    for directory in os.listdir(os.path.abspath("../")) :
+        if(directory not in NOT_ALLOWED and directory == folder) :
+            abs_path = "../{}".format(directory)
+            for file in os.listdir(abs_path):
+                formatted_name = standard_format(file).split(".")[0]
+                new_name = "{}_{}.{}".format(folder, formatted_name, extension)
 
-            os.rename(os.path.join(abs_path, file), os.path.join(abs_path, new_name))
+                # os.rename(os.path.join(abs_path, file), os.path.join(abs_path, new_name))
+                print(new_name)
+
+def standard_format(s) :
+    return s.replace(" ", "_")\
+            .replace("-", "_")\
+            .replace(",", "")\
+            .lower()
+
+
+def RenameLeetCodeDirectory() :
+    folder = "LeetCode"
+    extension = "py"
+    rename_files(folder, extension)
+
+if __name__ == "__main__" :
+    RenameLeetCodeDirectory()
