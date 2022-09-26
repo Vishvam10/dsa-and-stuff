195c4eaba8a3f9e5b2cbb53a734dfb98e2b94dd1
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-26 23:57:23 +0530

    Added 191, 1281

---
+++ LeetCode/LeetCode_1281.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        prod = 1
+        s = 0
+        
+        while n:
+            n, remainder = divmod(n, 10)
+            prod *= remainder            
+            s += remainder
+            
+        return prod - s
