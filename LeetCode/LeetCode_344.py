55680276d2821dc3a00bd34dc7e744364cfa0a0e
Vishvam10 <s.vishvam1025@gmail.com>
2022-09-27 21:49:40 +0530

    Added LeetCode : 344, 557, 566

---
+++ LeetCode/LeetCode_344.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, arr: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        i, j = 0, len(arr) - 1
+        while(i <= j) :
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+            i += 1
+            j -= 1
+        
