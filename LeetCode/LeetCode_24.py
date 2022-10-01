dd7a6520211100b5357f85770e84811b62ef6e51
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-01 19:48:51 +0530

    Added LeetCode : 24

---
+++ LeetCode/LeetCode_24.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if(head is None or head.next is None) :
+            return head
+        dummy = ListNode(-1)
+        prev = dummy
+        cur = head
+        while(cur and cur.next) :
+            prev.next = cur.next
+            # Here, we assign cur.next using prev's pointers because 
+            # if we used cur's next pointers, then we would need to 
+            # check if cur.next.next exists. 
+            cur.next = prev.next.next
+            prev.next.next = cur
+            
+            prev = cur
+            cur = cur.next
+        return dummy.next
