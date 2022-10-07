bd23192ef4e16b0d8b87ad23ebcf72e0c2365c2a
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-07 06:32:04 +0530

    Added LeetCode : 2259, 2423

---
+++ LeetCode/LeetCode_2259.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDigit(self, number: str, digit: str) -> str:
+        ans = []
+        for i in range(len(number)):
+            if number[i] == digit:
+                ans.append("".join(number[:i]+number[i+1:]))
+        ans.sort()
+        return ans[len(ans) - 1]
+                
+        
---
+++ LeetCode/LeetCode_2423.py
@@ -0,0 +1,23 @@
+class Solution:
+    # Simulate the problem, the constraints are small
+    def equalFrequency(self, word: str) -> bool:
+        mp = Counter(word)
+        for i in range(len(word)) :
+            ch = word[i]
+            
+            # Temporarily delete the element
+            mp[ch] -= 1
+            
+            # Permanently remove if frequency is 0
+            if(mp[ch] == 0) :
+                mp.pop(ch)
+        
+            if(len(set(mp.values())) == 1) :
+                return True
+            
+            # Add the element back
+            mp[ch] += 1
+            
+        return False
+        
+        
