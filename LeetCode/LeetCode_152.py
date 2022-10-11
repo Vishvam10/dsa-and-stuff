95c0f80cf38c235a1d1a97be572377c15d1cf4f0
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-11 22:48:32 +0530

    Added  LeetCode : 152

---
+++ LeetCode/LeetCode_152.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProduct(self, arr: List[int]) -> int:
+        prod = 1
+        ans = float("-inf")
+        
+        for i in range(len(arr)) :
+            prod *= arr[i]
+            ans = max(ans, prod)
+            if(prod == 0) :
+                prod = 1
+                
+        prod = 1
+        
+        for i in range(len(arr) - 1, -1, -1) :
+            prod *= arr[i]
+            ans = max(ans, prod)
+            if(prod == 0) :
+                prod = 1
+                
+        return ans
