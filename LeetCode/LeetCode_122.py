38edd61c55cd94c3de0b2292681a5fd2e5588f59
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-12 18:33:50 +0530

    Added  LeetCode : 122

---
+++ LeetCode/LeetCode_122.py
@@ -0,0 +1,19 @@
+class Solution:
+    # The action we can do on ith day is either buy 
+    # (if last action is sell), or sell (if last action 
+    # is buy), or do nothing (not buy, not sell).
+    def maxProfit(self, arr: List[int]) -> int:
+        curBuy = float("-inf")
+        curSold = float("-inf")
+        lastBuy = -arr[0]
+        lastSold = 0
+        n = len(arr)
+        if(n == 0) :
+            return 0
+        for i in range(1, n) :
+            curBuy = max(lastBuy, lastSold - arr[i])
+            curSold = max(lastSold, lastBuy + arr[i])
+            lastBuy = curBuy
+            lastSold = curSold
+        return lastSold
+        
