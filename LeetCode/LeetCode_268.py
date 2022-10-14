f97e8e6335f6faa0c1b2328d85088afb952281c0
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-14 19:30:54 +0530

    Added  LeetCode : 268

---
+++ LeetCode/LeetCode_268.py
@@ -0,0 +1,10 @@
+"""
+DIFFICULTY : easy
+TAGS : array, hash table, sorting, binary search, bit manipulatiom, math
+"""
+class Solution:
+    # ALTERNATE : Apply XOR operation to
+    # both the index and value of the array.
+    def missingNumber(self, arr: List[int]) -> int:
+        n = len(arr)
+        return n * (n+1) // 2 - sum(arr)
