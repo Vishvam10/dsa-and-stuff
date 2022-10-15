388e54de255e21641b058acef54843cf491bd752
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-15 14:53:53 +0530

    Added  LeetCode : 384

---
+++ LeetCode/LeetCode_384.py
@@ -0,0 +1,27 @@
+"""
+DIFFICULTY : medium
+TAGS : array, math, randomized
+"""
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.cpy = nums[:]
+        self.arr = nums[:]
+
+    def reset(self) -> List[int]:
+        self.arr = self.cpy.copy()
+        return self.arr
+
+    def shuffle(self) -> List[int]:
+        n = len(self.arr)
+        for i in range(n):
+            j = randint(i, n-1)
+            self.arr[i], self.arr[j] = self.arr[j], self.arr[i]
+        return self.arr
+            
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
