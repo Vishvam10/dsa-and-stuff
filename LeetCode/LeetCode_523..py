d75634b7ef5cc91fc955e7d342e8cce3db1e22e3
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-26 23:55:29 +0530

    Added  LeetCode : 523

---
+++ LeetCode/LeetCode_523..py
@@ -0,0 +1,18 @@
+"""
+DIFFICULTY : medium
+TAGS : array, hash table, math, prefix sum
+"""
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        # initialize the hash map with index 0 for sum 0
+        mp = {0: 0}
+        s = 0
+        for i in range(len(nums)):
+            s += nums[i]
+            # if the remainder s % k occurs for the first time
+            if s % k not in mp:
+                mp[s % k] = i + 1
+            # if the subarray size is at least two
+            elif mp[s % k] < i:
+                return True
+        return False
