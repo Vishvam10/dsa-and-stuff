3307407621712a905aa1c7bae0a29220f44e27fc
Vishvam10 <s.vishvam1025@gmail.com>
2022-10-28 17:08:55 +0530

    Added  LeetCode : 209

---
+++ LeetCode/LeetCode_209..py
@@ -0,0 +1,21 @@
+"""
+DIFFICULTY : medium
+TAGS : array, binary search, sliding window, prefix sum
+"""
+class Solution:
+    def minSubArrayLen(self, target: int, arr: List[int]) -> int:
+        n = len(arr)
+        left = 0
+        ans = float("inf")
+        s = 0
+        for i in range(n) :
+            s += arr[i]
+            while(s >= target) :
+                ans = min(ans, i - left + 1)
+                s -= arr[left]
+                left += 1
+        
+        if(ans == float("inf")) :
+            return 0
+        return ans
+            
