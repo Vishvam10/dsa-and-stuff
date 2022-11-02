f2b57bfb88f16cc3fbf007ca8aca504c73211cc9
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-02 21:24:09 +0530

    Added  LeetCode : 2418

---
+++ LeetCode/LeetCode_2418..py
@@ -0,0 +1,12 @@
+"""
+DIFFICULTY : easy
+TAGS : array, string, hash table, sorting
+"""
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        ans = []
+        for i in range(len(names)):
+            ans.append([heights[i],names[i]])
+        ans = sorted(ans, reverse=True)
+        return [name for height, name in ans]
+        
