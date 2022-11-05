d8be7ce1b6b88b597d32aa3302a396fce572a28e
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-05 14:10:01 +0530

    Added  LeetCode : 2443

---
+++ LeetCode/LeetCode_2443..py
@@ -0,0 +1,19 @@
+"""
+DIFFICULTY : medium
+TAGS : math, enumeration
+"""
+class Solution:
+    def sumOfNumberAndReverse(self, n: int) -> bool:
+        def rev(n) :
+            ans = 0
+            while(n) :
+                ans = (ans * 10) + n % 10
+                n //= 10
+            return ans
+        
+        for i in range(n//2, n+1) :
+            if(i + rev(i) == n) :
+                return True
+        return False
+                    
+        
