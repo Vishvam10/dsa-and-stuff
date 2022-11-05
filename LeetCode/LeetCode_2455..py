c7f5578d47b40f9e070dd6ea2d97ee9bc82cf6c0
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-05 12:00:15 +0530

    Added  LeetCode : 2455

---
+++ LeetCode/LeetCode_2455..py
@@ -0,0 +1,16 @@
+"""
+DIFFICULTY : easy
+TAGS : array, math
+"""
+class Solution:
+    def averageValue(self, arr: List[int]) -> int:
+        s = 0
+        count = 0
+        for x in arr :
+            if(x % 2 == 0 and x % 3 == 0) :
+                s += x
+                count += 1
+        if(count == 0) :
+            return 0
+        return s // count
+        
