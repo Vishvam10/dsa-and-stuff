ddf91a974b735aa65ffa6fea3a821fdd33bb4935
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-05 13:12:44 +0530

    Added  LeetCode : 2441, 2442, 2457

---
+++ LeetCode/LeetCode_2441..py
@@ -0,0 +1,21 @@
+"""
+DIFFICULTY : easy
+TAGS : array, hash table
+"""
+class Solution:
+    def findMaxK(self, arr: List[int]) -> int:
+        arr.sort()
+        left = 0
+        right = len(arr) - 1
+        ans = -1
+        while(left < right) :
+            if(arr[left] + arr[right] == 0) :
+                ans = max(ans, arr[right])
+                left += 1
+                right -= 1
+            elif(arr[left] + arr[right] > 0) :
+                right -= 1
+            else :
+                left += 1
+            
+        return ans 
---
+++ LeetCode/LeetCode_2457..py
@@ -0,0 +1,26 @@
+"""
+DIFFICULTY : medium
+TAGS : math, greedy
+"""
+class Solution:
+    # IDEA : Go from right (say n-1 == i), 
+    # for every iteration, increase the next (i+1 == n-2) 
+    # digit by 1 and set everything before it to 0
+
+    # In other words, turn the rightmost non-zero digit 
+    # to zero until the digit sum is greater than target.
+    def makeIntegerBeautiful(self, n: int, target: int) -> int:
+        def ds(n) :
+            s = 0
+            while(n) :
+                s += n % 10
+                n //= 10
+            return s
+        temp = n
+        i = 0
+        while ds(n) > target:
+            # Not just n // 10. For example, 123456 -> 12346. To do so,
+            # we need n // 10 = 12345 then +1 to give us 12346
+            n = n // 10 + 1
+            i += 1
+        return n * (10 ** i) - temp
