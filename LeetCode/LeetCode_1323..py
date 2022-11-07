6f87c60a3da0f69dedfc79eedcd7aed94b3a5d5f
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-07 19:26:16 +0530

    Added  LeetCode : 1323

---
+++ LeetCode/LeetCode_1323..py
@@ -0,0 +1,19 @@
+"""
+DIFFICULTY : easy
+TAGS : math, greedy
+"""
+class Solution:
+    def maximum69Number (self, n: int) -> int:
+        m = n
+        ind = -1
+        curr = 0
+        while(m) :
+            if(m % 6 == 0) :
+                ind = curr
+            m //= 10
+            curr += 1
+        
+        if(ind == -1) :
+            return n
+        else :
+            return n + 3 * (10**ind)
