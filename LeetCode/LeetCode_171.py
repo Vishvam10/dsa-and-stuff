8d0da0018c4a7042d65768f2a8a1d76d07adfb6f
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-18 20:08:52 +0530

    Added  LeetCode : 171

---
+++ LeetCode/LeetCode_171.py
@@ -0,0 +1,10 @@
+"""
+DIFFICULTY : easy
+TAGS : math, string
+"""
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        ans = 0
+        for ch in s :
+            ans = (ord(ch) - 64) + 26 * ans
+        return ans
