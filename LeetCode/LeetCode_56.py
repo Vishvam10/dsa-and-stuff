4536d810d84797b66b626cb29ee40b57c43fc49c
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-21 21:47:40 +0530

    Added  LeetCode : 56, 57

---
+++ LeetCode/LeetCode_56.py
@@ -0,0 +1,15 @@
+"""
+DIFFICULTY : medium
+TAGS : array
+"""
+class Solution:
+    def merge(self, arr: List[List[int]]) -> List[List[int]]:
+        arr = sorted(arr, key = lambda x: x[0])
+        ans = []
+        for ele in arr :
+            if(not ans or ans[-1][1] < ele[0]) :
+                ans.append(ele)
+            else :
+                ans[-1][1] = max(ans[-1][1], ele[1])
+        
+        return ans
---
+++ LeetCode/LeetCode_57.py
@@ -0,0 +1,13 @@
+class Solution:
+    def insert(self, arr: List[List[int]], b: List[int]) -> List[List[int]]:
+        arr.append(b)
+        arr.sort(key=lambda x: x[0])
+        ans = []
+
+        for ele in arr:
+            if(not ans or ele[0] > ans[-1][1]) :
+                ans.append(ele)
+            else :
+                ans[-1][1] = max(ans[-1][1], ele[1])
+        
+        return ans
