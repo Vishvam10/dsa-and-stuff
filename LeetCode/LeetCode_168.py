3030d96e913dfe2fd29928b358cbf9d3085f1ce7
Vishvam10 <s.vishvam1025@gmail.com>
2022-11-26 20:27:32 +0530

    Added  LeetCode : 168

---
+++ LeetCode/LeetCode_168.py
@@ -0,0 +1,12 @@
+"""
+DIFFICULTY : easy
+TAGS : math, string
+"""
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        ans = []
+        while n:
+            n, r = divmod(n - 1, 26)
+            ans.append(chr(r + ord("A")))
+        return "".join(reversed(ans))
+        
