7dbd76e260bda1f276b99f78bae7bfa6e21a720c
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-06 21:51:42 +0530

    Added  LeetCode : 2460

---
+++ LeetCode/LeetCode_2460.py
@@ -0,0 +1,25 @@
+"""
+DIFFICULTY : easy
+TAGS : array, simulation
+"""
+class Solution:
+    def applyOperations(self, arr: List[int]) -> List[int]:
+        
+        n = len(arr)
+        i = 0
+        while(i < n-1):
+            if arr[i] == arr[i+1]:
+                arr[i] *= 2
+                arr[i+1] = 0
+                i += 2
+            else:
+                i += 1
+
+        last_zero_ind = 0
+        
+        for i, val in enumerate(arr):
+            if val != 0:
+                arr[last_zero_ind], arr[i] = arr[i], arr[last_zero_ind]
+                last_zero_ind += 1
+        
+        return arr
