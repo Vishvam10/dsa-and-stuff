69632e76392bbb9638938dc0e8c3c544c44cbf9d
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-07 21:23:40 +0530

    Added  LeetCode : 1985

---
+++ LeetCode/LeetCode_1985.py
@@ -0,0 +1,10 @@
+"""
+DIFFICULTY : easy
+TAGS : array, string, divide and conquer, sorting, heap (priority queue), quickselect
+"""
+class Solution:
+    def kthLargestNumber(self, arr: List[str], k: int) -> str:
+        arr.sort(key=lambda x: int(x), reverse=True)
+        if(k == len(arr)) :
+            return str(arr[-1])
+        return str(arr[k-1])        
