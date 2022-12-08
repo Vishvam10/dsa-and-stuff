58d34f9a9a998d5a70ed090b32b38ea35750dfe4
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-08 19:37:54 +0530

    Added  LeetCode : 102

---
+++ LeetCode/LeetCode_102.py
@@ -0,0 +1,32 @@
+"""
+DIFFICULTY : medium
+TAGS : tree, bfs, binary tree
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if(not root) :
+            return []
+        depth = 0
+        queue = []
+        queue.append(root) 
+        ans = []
+        while(queue) :
+            # depth += 1
+            temp = []
+            for _ in range(len(queue)) :
+                node = queue.pop(0)
+                if(node) :
+                    temp.append(node.val)
+                    if(node.left) :
+                        queue.append(node.left)
+                    if(node.right) :
+                        queue.append(node.right) 
+            ans.append(temp)
+
+        return ans
