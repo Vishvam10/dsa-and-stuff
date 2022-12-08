5e3cb77c04d9eaebc13440e2d8c47cb32344bb3e
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-08 19:28:05 +0530

    Added  LeetCode : 111

---
+++ LeetCode/LeetCode_111.py
@@ -0,0 +1,28 @@
+"""
+DIFFICULTY : easy
+TAGS : tree, dfs, bfs, binary tree
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        depth = 0
+        if not root:
+            return 0
+        queue = []
+        queue.append(root)
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                if not node.left and not node.right:
+                    return depth
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return depth
