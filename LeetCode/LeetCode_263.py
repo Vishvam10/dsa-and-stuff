6fa244bb8e2205586b417d626293b0540e679080
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-10 00:08:43 +0530

    Added  LeetCode : 263

---
+++ LeetCode/LeetCode_263.py
@@ -0,0 +1,23 @@
+"""
+DIFFICULTY : easy
+TAGS : math
+"""
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if(n <= 0) :
+            return False
+        while(n > 1) :
+            if(n % 2 == 0) :
+                n //= 2
+            elif(n % 3 == 0) :
+                n //= 3
+            elif(n % 5 == 0) :
+                n //= 5
+            else :
+                return False
+        return True
+    
+        # ALTERNATE : return n > 0 == 30**32 % n
+            
+                    
+        
