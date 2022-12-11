0d78d2a3fb8dcafc141ae01cd8331f88c5051eeb
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-11 22:58:01 +0530

    Added  LeetCode : 1356

---
+++ LeetCode/LeetCode_1356.py
@@ -0,0 +1,8 @@
+"""
+DIFFICULTY : easy
+TAGS : array, bit manipulation, sorting, counting
+"""
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        return sorted(arr, key=lambda x: [bin(x).count("1"), x])
+        
