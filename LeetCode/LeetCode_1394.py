d91b23ca7f6a0e7bdb2a700ae862b15d963e5b14
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-16 22:56:17 +0530

    Added  LeetCode : 1394

---
+++ LeetCode/LeetCode_1394.py
@@ -0,0 +1,18 @@
+"""
+DIFFICULTY : easy
+TAGS : array, counting, hash table
+"""
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        mp = {}
+        ans = -1
+        for x in arr :
+            if(x not in mp) :
+                mp[x] = 1
+            else :
+                mp[x] += 1
+        for k,v in mp.items() :
+            if(k == v) :
+                ans = max(ans, v)
+                
+        return ans
