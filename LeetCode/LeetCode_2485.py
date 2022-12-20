7aeeb2ee3542d82fc955feb9d35a8cb59924cf7d
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-20 22:11:53 +0530

    Added  LeetCode : 2485

---
+++ LeetCode/LeetCode_2485.py
@@ -0,0 +1,24 @@
+"""
+DIFFICULTY : easy
+TAGS : math, prefix sum
+"""
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        s = (n * (n+1)) // 2
+        low = 1;  
+        high = n
+        while low <= high:
+            mid = (low + high) // 2
+            cur = (mid * (mid+1))//2
+            key = s - cur + mid
+            if cur == key:
+                return mid
+            elif cur < key:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+        
+            
+            
+            
