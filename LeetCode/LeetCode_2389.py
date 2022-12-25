72fb11867dfea18dc91b3ccfb768aa3f992664a3
Vishvam10 <s.vishvam1025@gmail.com>
2022-12-25 09:59:02 +0530

    Added  LeetCode : 2389

---
+++ LeetCode/LeetCode_2389.py
@@ -0,0 +1,20 @@
+"""
+DIFFICULTY : easy
+TAGS : array, binary search, greedy, sorting, prefix sum
+"""
+class Solution:
+    def answerQueries(self, arr: List[int], queries: List[int]) -> List[int]:
+        arr.sort()
+        ans = []
+        for target in queries :
+            temp = 0
+            count = 0
+            for val in arr :
+                if(temp + val <= target) :
+                    temp += val
+                    count += 1
+                else :
+                    break         
+            ans.append(count)
+
+        return ans
