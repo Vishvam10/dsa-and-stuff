a5b87bacf4cadf7122ce3127c0e04b582bc9a6b7
Vishvam10 <s.vishvam1025@gmail.com>
2023-01-04 14:26:37 +0530

    Added  LeetCode : 2133

---
+++ LeetCode/LeetCode_2133.py
@@ -0,0 +1,19 @@
+"""
+DIFFICULTY : easy
+TAGS : array, hash table, matrix
+"""
+class Solution:
+    def checkValid(self, mat: List[List[int]]) -> bool:
+        n = len(mat)
+        for x in range(n) :
+            # Can also use normal arrays : [0] * (n+1)
+            row = bytearray(n + 1)
+            col = bytearray(n + 1)
+            for y in range(n) :
+                r, c = mat[x][y], mat[y][x]
+                row[r] += 1
+                col[c] += 1
+                if(row[r] > 1 or col[c] > 1) :
+                    return False
+        return True
+                
