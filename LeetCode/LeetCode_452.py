5a593f0b1913e4fd85ec1924d388b919cb123189
Vishvam10 <s.vishvam1025@gmail.com>
2023-01-05 10:52:17 +0530

    Added  LeetCode : 452

---
+++ LeetCode/LeetCode_452.py
@@ -0,0 +1,15 @@
+"""
+DIFFICULTY : medium
+TAGS : array, greedy, sorting
+"""
+class Solution:
+    def findMinArrowShots(self, pts: List[List[int]]) -> int:
+        pts.sort(key=lambda x: x[1])
+        cur = pts[0][1]
+        count = 1
+        for (st, end) in pts :
+            if(st > cur) :
+                cur = end
+                count += 1
+        
+        return count
