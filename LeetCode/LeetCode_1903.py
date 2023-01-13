4fbb37400ba75c4810e5b01ee366a087b5688edf
Vishvam10 <s.vishvam1025@gmail.com>
2023-01-13 23:49:13 +0530

    tMessage.py Added  LeetCode : 1903

---
+++ LeetCode/LeetCode_1903.py
@@ -0,0 +1,17 @@
+"""
+DIFFICULTY : easy
+TAGS : math, string, greedy
+"""
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        n = len(num)
+        left = 0
+        right = n - 1
+        while(right >= 0) :
+            if(int(num[right]) % 2 == 0) :
+                right -= 1
+            else :
+                break
+        if(right < 0) :
+            return ""
+        return num[:right+1]
