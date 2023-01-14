3937310201f4751048b3b0916c2136abf90c41c7
Vishvam10 <s.vishvam1025@gmail.com>
2023-01-14 22:33:51 +0530

    Added  LeetCode : 2520

---
+++ LeetCode/LeetCode_2520.py
@@ -0,0 +1,14 @@
+"""
+DIFFICULTY : easy
+TAGS : math
+"""
+class Solution:
+    def countDigits(self, n: int) -> int:
+        ans = 0
+        temp = n
+        while(n > 0) :
+            digit = n % 10
+            if(temp % digit == 0) :
+                ans += 1
+            n //= 10
+        return ans
