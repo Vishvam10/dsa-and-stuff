24ed0bb5f5de9a48b0023967b66ae9e9a248a7e3
Vishvam10 <s.vishvam1025@gmail.com>
2023-01-16 21:48:48 +0530

    Added  LeetCode : 230

---
+++ LeetCode/LeetCode_230.py
@@ -0,0 +1,21 @@
+"""
+DIFFICULTY : medium
+TAGS : tree, dfs, bst, binary tree
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # IDEA : The inorder traversal (LNR) of a BST gives
+    #        a sorted array in ascending order
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+       
+        def helper(node) :
+            if(not node) :
+                return []
+            return helper(node.left) + [node.val] + helper(node.right)
+        
+        return helper(root)[k - 1] 	
