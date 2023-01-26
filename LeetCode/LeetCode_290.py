5ad7a30d10503d809d315d54b3231369af1db270
Vishvam10 <s.vishvam1025@gmail.com>
2023-01-26 23:11:48 +0530

    Added  LeetCode : 290

---
+++ LeetCode/LeetCode_290.py
@@ -0,0 +1,16 @@
+class Solution:
+    # ALTERNATE : (Literally bijection)
+    #   Use 2 maps - one for map b/w pattern and s, the other
+    #   for a map b/w s and pattern
+
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+
+        pattern_counter = Counter(pattern)
+        words_counter = Counter(words)
+        pair_counter = Counter(zip(pattern, words))
+
+        return (
+            (len(pattern_counter) == len(words_counter) == len(pair_counter)) and 
+            (len(pattern) == len(words))
+        )
