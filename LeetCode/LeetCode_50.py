b127df805aefde88e46ff180d1603029cae73532
Vishvam10 <s.vishvam1025@gmail.com>
2023-01-28 23:08:09 +0530

    Added  LeetCode : 50

---
+++ LeetCode/LeetCode_50.py
@@ -0,0 +1,19 @@
+"""
+DIFFICULTY : easy
+TAGS : math, recursion
+"""
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if((n == 0) or (x == 1)) :
+            return 1
+            
+        if(x == 0) :
+            return 0
+        
+        if(n < 0) :
+            return self.myPow(1/x, abs(n))
+        
+        if(n % 2 == 0) :
+            return self.myPow(x*x, n // 2)
+        else :
+            return x*self.myPow(x*x, n // 2)
