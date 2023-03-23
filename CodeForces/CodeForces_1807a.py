b839718587979e04b09d502bcaa64c1ebcad48c6
Vishvam10 <s.vishvam1025@gmail.com>
2023-03-23 23:59:58 +0530

    Added  CodeForces : 1807a

---
+++ CodeForces/CodeForces_1807a.py
@@ -0,0 +1,22 @@
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    return(input().strip())
+
+def solve() :
+    [a, b, c] = inlt()
+    if(a + b == c) :
+        print("+")
+        return
+    elif(a - b == c) :
+        print("-")
+        return
+
+   
+
+t = inp()
+
+for _ in range(t) :
+    solve()
