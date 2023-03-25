71357af74cc895ed24b22e912a3eb93e812f4676
Vishvam10 <s.vishvam1025@gmail.com>
2023-03-25 22:38:20 +0530

    Added  CodeForces : 1800d

---
+++ CodeForces/CodeForces_1800d.py
@@ -0,0 +1,20 @@
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    return(input().strip())
+
+def solve() :
+    n = inp()
+    s = insr()
+    ans = n - 1
+    for i in range(1, n-1, 1) :
+        if(s[i-1] == s[i+1]) :
+            ans -= 1
+    print(ans)
+   
+t = inp()
+
+for _ in range(t) :
+    solve()
