3975aeb9c086423d92bcc2deff95be268b1a10f1
Vishvam10 <s.vishvam1025@gmail.com>
2023-03-27 00:09:09 +0530

    Added  CodeForces : 479a

---
+++ CodeForces/CodeForces_479a.py
@@ -0,0 +1,20 @@
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    return(input().strip())
+
+def solve() :
+    a = inp()
+    b = inp()
+    c = inp()
+    ans = a + b + c
+    ans = max(ans, (a + b) * c)
+    ans = max(ans, a * (b + c))
+    ans = max(ans, a * b * c)
+
+    print(ans)
+    return
+
+solve()
