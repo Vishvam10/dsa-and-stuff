3f73d900d4f8f5697b69bd8cbc69ad283680236e
Vishvam10 <s.vishvam1025@gmail.com>
2023-04-06 23:30:26 +0530

    Added  CodeForces : 1095b

---
+++ CodeForces/CodeForces_1095b.py
@@ -0,0 +1,22 @@
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    return(input().strip())
+
+def solve() :
+
+    n = inp()
+    arr = inlt()
+    arr.sort()
+
+    if(n == 1 or n == 2) :
+        print(0)
+        return
+    
+    print(min(arr[n-2] - arr[0], arr[n-1] - arr[1]))
+    return
+
+
+solve()
