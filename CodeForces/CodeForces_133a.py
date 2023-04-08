f19461d44a589c8e431007d03e066dbf37be70de
Vishvam10 <s.vishvam1025@gmail.com>
2023-04-08 23:09:06 +0530

    Added  CodeForces : 133a

---
+++ CodeForces/CodeForces_133a.py
@@ -0,0 +1,14 @@
+def insr():
+    return(input().strip())
+
+def solve() :
+    s = insr()
+    
+    for ch in s :
+        if(ch == "H" or ch == "Q" or ch == "9") :
+            print("YES")
+            return
+    print("NO")
+    return
+
+solve()
