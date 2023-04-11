5087c7ca14e2d1647cb61541140f08ec0c93f515
Vishvam10 <s.vishvam1025@gmail.com>
2023-04-11 23:39:30 +0530

    Added  CodeForces : 1811a

---
+++ CodeForces/CodeForces_1811a.py
@@ -0,0 +1,24 @@
+def inp():
+    return(int(input()))
+
+def insr():
+    return(input().strip())
+
+
+def solve() :
+    [n,d] = input().split()
+    s = insr()
+
+    i = 0
+
+    while(i < int(n) and s[i] >= d) :
+        i += 1
+    
+    print(s[:i] + str(d) + s[i:])
+    return
+
+        
+t = inp()
+
+for _ in range(t) :
+    solve()
