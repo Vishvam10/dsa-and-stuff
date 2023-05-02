9ff83a2a67865aa7430139454f773cbe16f10493
Vishvam10 <s.vishvam1025@gmail.com>
2023-05-03 00:20:54 +0530

    Added  CodeStudio : maximum_sum_rectangle

---
+++ CodeStudio/CodeStudio_maximum_sum_rectangle.py
@@ -0,0 +1,34 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def kadane(arr, n) :
+    curSum = 0
+    maxSum = float("-inf")
+
+    for i in range(n) :
+        curSum += arr[i]
+        maxSum = max(maxSum, curSum)
+
+        if(curSum < 0) :
+            curSum = 0
+
+    return maxSum    
+
+def maxSumRectangle(arr, n, m):
+
+    curSum = 0
+    maxSum = float("-inf")
+    temp = []
+  
+    for i in range(m) :
+        temp = [0 for _ in range(n)]
+        for j in range(i, m) :
+            for x in range(n) :
+                temp[x] += arr[x][j]
+            
+            curSum = kadane(temp, n)
+            maxSum = max(maxSum, curSum)
+    
+    return maxSum
