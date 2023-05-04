58e993a3f3c572ff56b221bb3192944482a1b3eb
Vishvam10 <s.vishvam1025@gmail.com>
2023-05-04 16:37:04 +0530

    Added  GeeksForGeeks : check_if_subarray_with_given_product_exists_in_an_array, split_an_array_into_two_equal_sum_subarrays, subarray_of_size_k_with_given_sum, Subarray with given sum

---
+++ GeeksForGeeks/GeeksForGeeks_check_if_subarray_with_given_product_exists_in_an_array.py
@@ -0,0 +1,35 @@
+
+
+def subarray_with_given_product(arr, n, prod) :
+
+    minVal, maxVal, maxProd = arr[0], arr[0], arr[0]
+
+    for i in range(1, n) :
+
+        # Swap minVal and maxVal when multiplied 
+        # by a negative number
+        if(arr[i] < 0) :
+            maxVal, minVal = minVal, maxVal
+
+        maxVal = min(arr[i], maxVal * arr[i])
+        minVal = min(arr[i], minVal * arr[i])
+
+        if(minVal == prod or maxVal == prod) :
+            return True
+    
+        maxProd = max(maxProd, maxVal)
+
+    return False
+
+
+
+arr = [ 1, 2, -5, -4 ]
+product = -10
+n = len(arr)
+
+print(subarray_with_given_product(arr, n, product))
+
+
+
+
+
