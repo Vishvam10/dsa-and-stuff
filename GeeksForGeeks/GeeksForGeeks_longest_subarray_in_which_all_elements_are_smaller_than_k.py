79b565654cae5dbf0da5e0d156603f627e817152
Vishvam10 <s.vishvam1025@gmail.com>
2023-05-04 19:26:35 +0530

    Added  GeeksForGeeks : longest_subarray_in_which_all_elements_are_smaller_than_k, sum_of_all_subarrays_of_size_k

---
+++ GeeksForGeeks/GeeksForGeeks_longest_subarray_in_which_all_elements_are_smaller_than_k.py
@@ -0,0 +1,41 @@
+
+
+def largest_subarray_with_all_elements_smaller_than_k(arr, n, k) :
+
+    c = 0
+    start = 0
+    end = 0
+    max_len = -1
+
+    for i in range(n) :
+        if(arr[i] < k) :
+            c += 1
+            end += 1
+        else :
+            if c > max_len:
+                max_len = c
+                start = i - c
+            c = 0
+            end = i-1
+
+
+    if(c != 0) :
+        if c > max_len:
+            max_len = c
+            start = n - c
+        end = n-1
+
+    return [max_len, start, end]
+
+
+
+arr = [1, 8, 3, 5, 2, 2, 1, 13, 3, 5, 2, 2, 1, 1, 0, 12]
+N = len(arr)
+K = 6
+ 
+print(largest_subarray_with_all_elements_smaller_than_k(arr, N, K))
+
+
+
+
+
---
+++ GeeksForGeeks/GeeksForGeeks_sum_of_all_subarrays_of_size_k.py
@@ -0,0 +1,24 @@
+
+
+
+def sum_of_all_subarrays(arr, n, size) :
+
+    ans = []    
+    s = sum(arr[:k])
+
+    ans.append(s)
+
+    for i in range(k, n) :
+
+        # arr[end] - arr[start]
+        s += (arr[i] - arr[i-k])
+        ans.append(s)
+
+    return ans
+
+
+arr = [1, -2, 3, -4, 5, 6]
+n = len(arr)
+k = 2
+
+assert [-1, 1, -1, 1, 11] == sum_of_all_subarrays(arr, n, k)
