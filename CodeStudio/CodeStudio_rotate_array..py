bd268f5b819b5b23f58dc19e8bf7a99e4014a76f
Vishvam10 <s.vishvam1025@gmail.com>
2023-07-19 10:26:39 +0530

    Added CodeStudio : left_rotate_array_by_one, rotate_array

---
+++ CodeStudio/CodeStudio_rotate_array..py
@@ -0,0 +1,20 @@
+import copy
+
+def inlt():
+    return(list(map(int,input().split())))
+
+n = int(input())
+arr = inlt()
+k = int(input())
+direction = 'l'
+
+cpy = copy.deepcopy(arr)
+if(direction == 'l') :
+    for i in range(n) :
+        arr[(i-k)%n] = cpy[i]
+elif(direction == 'r') :
+    for i in range(n) :
+        arr[(i+k)%n] = cpy[i]
+
+for x in arr :
+    print(x, end=' ')
