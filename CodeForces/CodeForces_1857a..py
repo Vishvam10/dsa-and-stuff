631d5878eb9454b268a48c9f12fa0d7761a5a4e7
Vishvam10 <s.vishvam1025@gmail.com>
2023-08-10 21:50:41 +0530

    Added CodeForces : 1857a | Modified LeetCode : 118

---
+++ CodeForces/CodeForces_1857a..py
@@ -0,0 +1,22 @@
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+
+def solve() :
+    n = inp()
+    arr = inlt()
+    s = sum(arr)
+    if(s % 2 == 0) :
+        print('YES')
+        return
+    else :
+        print('NO')
+        return
+
+
+
+t = inp()
+
+for _ in range(t) :
+    solve()
--- LeetCode/LeetCode_118.py
+++ LeetCode/LeetCode_118.py
@@ -1,8 +1,16 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        ans = [[1]]
-        for i in range(1, numRows):
-            temp1 = ans[-1] + [0]
-            temp2 = [0] + ans[-1]
-            ans.append([temp1[i]+temp2[i] for i in range(len(temp1))])
-        return ans[:numRows]
+    def generate(self, nrows: int) -> List[List[int]]:
+        ans = []
+        ans.append([1])
+        depth = 1
+        while(depth < nrows) :
+            temp = []
+            temp.append(1)
+            n = len(ans[len(ans)-1])
+            for i in range(1, n, 1) :
+                temp.append(ans[n-1][i-1] + ans[n-1][i])
+            temp.append(1)
+            ans.append(temp)
+            depth += 1
+
+        return ans
