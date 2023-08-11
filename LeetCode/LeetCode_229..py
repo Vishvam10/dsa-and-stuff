c14068d9f003849771f74e5c3118465b224c8d4f
Vishvam10 <s.vishvam1025@gmail.com>
2023-08-11 23:06:55 +0530

    Added CodeStudio : isomorphic_strings, print_pascals_triangle | LeetCode : 229

---
+++ LeetCode/LeetCode_229..py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+class Solution:
+    def majorityElement(self, arr: List[int]) -> List[int]:
+        n = len(arr)
+        if(n == 1) :
+            return arr
+       
+        mp = defaultdict(int)
+        ans = []
+        
+        for x in arr :
+            mp[x] += 1
+        
+        for k,v in mp.items() :
+            if(v > n//3) :
+                ans.append(k)
+        
+        # print(ans)
+
+        return ans
+            
+                    
+
