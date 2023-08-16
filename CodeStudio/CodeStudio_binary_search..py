ad7972ed10d7c16a2292eca01c18bc8af7614ab2
Vishvam10 <s.vishvam1025@gmail.com>
2023-08-16 23:07:06 +0530

    Added CodeStudio : binary_search

---
+++ CodeStudio/CodeStudio_binary_search..py
@@ -0,0 +1,14 @@
+def search(arr: [int], target: int):
+    n = len(arr)
+    left = 0
+    right = n-1
+    while(left <= right) :
+        mid = left + (right - left) // 2
+        if(arr[mid] == target) :
+            return mid
+        elif(arr[mid] < target) :
+            low = mid + 1
+        else :
+            high = mid - 1
+
+    return -1
