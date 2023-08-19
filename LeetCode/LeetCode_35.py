69a3eac8f704d48a01ed6ce6087bcb4d9eac4bb2
Vishvam10 <s.vishvam1025@gmail.com>
2023-08-20 00:28:15 +0530

    Added CodeStudio : implement_lower_bound, implement_upper_bound | Modified LeetCode : 35

--- LeetCode/LeetCode_35.py
+++ LeetCode/LeetCode_35.py
@@ -3,16 +3,22 @@ DIFFICULTY : easy
 TAGS : array, binary search
 """
 class Solution:
-    def searchInsert(self, arr: List[int], target: int) -> int:
-        low, high = 0, len(arr)
-        if(target > arr[len(arr)-1]) :
-            return len(arr)
-        while(low <= high) :
-            mid = low + (high - low)//2
-            if(arr[mid] == target) :
-                return mid
-            elif(arr[mid] < target) :
-                low = mid + 1
-            else :
-                high = mid - 1
-        return low
+    def searchInsert(self, arr: List[int], x: int) -> int:
+
+        def ub(arr, n, x) : 
+            left = 0
+            right = n - 1
+            ans = 0
+            while(left <= right) :
+                mid = left + (right - left) // 2
+                if(arr[mid] == x) :
+                    ans = mid
+                    left = mid + 1
+                elif(arr[mid] < x) :
+                    ans = mid + 1
+                    left = mid + 1
+                else :
+                    right = mid - 1
+            return ans
+        
+        return ub(arr, len(arr), x)
