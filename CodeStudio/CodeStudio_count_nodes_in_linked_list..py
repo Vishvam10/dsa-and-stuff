61e927702f15d76babac792a937adcafb7c61704
Vishvam10 <s.vishvam1025@gmail.com>
2023-09-05 15:32:50 +0530

    Added CodeStudio : count_nodes_in_linked_list, search_in_a_linked_list

---
+++ CodeStudio/CodeStudio_count_nodes_in_linked_list..py
@@ -0,0 +1,16 @@
+'''
+Following is the structure of the Node class already defined.
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+'''
+
+def length(head) :
+    n = 0
+    while(head) :
+        n += 1
+        head = head.next
+    return n
