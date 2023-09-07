f0bbdf0259d1c9f4a47b064b09a5e1095cf31bd5
Vishvam10 <s.vishvam1025@gmail.com>
2023-09-07 20:35:45 +0530

    Added LeetCode : 1283

---
+++ LeetCode/LeetCode_1283.py
@@ -0,0 +1,24 @@
+import math
+
+class Solution:
+    def smallestDivisor(self, arr: List[int], t: int) -> int:
+        n = len(arr)
+        if n > t:
+            return -1
+        low, high = 1, max(arr)
+
+        def sd(arr, div) :
+            s = 0
+            for i in range(len(arr)) :
+                s += math.ceil(arr[i] / div)
+            return s
+
+        while(low <= high) :
+            mid = low + (high - low) // 2
+            val = sd(arr, mid)
+            if(val <= t) :
+                high = mid - 1
+            else :
+                low = mid + 1
+        
+        return low
