246a6c2521f4b2ef8e4ddb78144c06bbd7bdac0d
Vishvam10 <s.vishvam1025@gmail.com>
2023-09-15 22:03:53 +0530

    Added LeetCode : 25 | Modified LeetCode : 61

---
+++ LeetCode/LeetCode_25.py
@@ -0,0 +1,53 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        def rev(start, end) :
+            prev = None
+            cur = start
+            while(cur != end) :
+                nxt = cur.next
+                cur.next = prev
+                prev = cur
+                cur = nxt
+            return prev
+        
+        def pl(node) :
+            while(node) :
+                print(node.val, ' -> ', end='')
+                node = node.next
+            return
+
+        dummy = ListNode(-1)
+        ans = dummy
+
+        temp = head
+        while(temp) :
+            start = temp
+            flag = True
+            c = 0
+            while(c < k and flag) :
+                if(temp) :
+                    temp = temp.next
+                    c += 1
+                else :
+                    flag = False
+            end = temp
+
+            if(c == k) :
+                rhead = rev(start, end)
+                dummy.next = rhead
+                for _ in range(k) :
+                    dummy = dummy.next
+            else :
+                # print('start, dummy, end : ', start, dummy, end)
+                dummy.next = start
+                dummy = dummy.next
+                
+        # pl(ans.next)
+
+        return ans.next
--- LeetCode/LeetCode_61.py
+++ LeetCode/LeetCode_61.py
@@ -1,31 +1,33 @@
-"""
-DIFFICULTY : medium
-TAGS : linked list, two pointers
-"""
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 class Solution:
-    # IDEA : Create a circular linked list and 
-    #        cut it at (n-k+1) position where n 
-    #        is the length of the linked list
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if not head:
+
+        def pl(node) :
+            while(node) :
+                print(node.val, ' -> ', end='')
+                node = node.next
+            return
+
+        if(head == None) : 
             return None
-        temp = head
+
+        temp, temp1 = head, head
         n = 1
-        while(temp.next):
-            temp = temp.next
+        while(temp.next) :
             n += 1
-        k = k % n
-        temp.next = head
-        temp1 = head
-        for i in range(n-k-1):
+            temp = temp.next 
+        k %= n
+
+        # Make it cyclic
+        temp.next = head 
+        for _ in range(n-k-1) :
             temp1 = temp1.next
-        
-        new_head = temp1.next
+        head = temp1.next
         temp1.next = None
-        
-        return new_head
+
+        return head
+
