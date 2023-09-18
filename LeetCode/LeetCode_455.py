588d3398ea19bb9b0434d5a79fc90668188e72b9
Vishvam10 <s.vishvam1025@gmail.com>
2023-09-18 21:40:32 +0530

    Added LeetCode : 455

---
+++ LeetCode/LeetCode_455.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        a, b, n, m = 0, 0, len(g), len(s)
+        while(a < n and b < m) :
+            if(s[b] >= g[a]) :
+                a += 1
+            b += 1
+        return a
+        
