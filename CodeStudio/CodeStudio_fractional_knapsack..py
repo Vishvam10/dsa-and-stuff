13a1e320457bfe25f693b7eb74f4082b4122ec97
Vishvam10 <s.vishvam1025@gmail.com>
2023-09-19 10:12:59 +0530

    Added CodeStudio : fractional_knapsack | LeetCode : 678

---
+++ CodeStudio/CodeStudio_fractional_knapsack..py
@@ -0,0 +1,20 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def maximumValue(items, n, w):
+
+	# Write your code here.
+	# ITEMS contains [weight, value] pairs.
+	items.sort(key=lambda x : x[1] / x[0], reverse=True)
+	ans = 0
+	for x in items :
+		if(x[0] < w) :
+			ans += (x[1])
+			w -= x[0]
+		else :
+			ans += (x[1]/x[0])*w
+			w = 0
+			break
+	return ans 
