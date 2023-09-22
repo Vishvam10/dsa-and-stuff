48052601162033639559633067f62ddbbe154da8
Vishvam10 <s.vishvam1025@gmail.com>
2023-09-22 06:23:18 +0530

    Added CodeStudio : get_all_unique_subsets | LeetCode : 90

---
+++ LeetCode/LeetCode_90.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetsWithDup(self, arr: List[int]) -> List[List[int]]:
+
+        def generate(arr, start, pref, acc) :
+            acc.append(pref)
+            
+            for i in range(start, len(arr)) :
+                if(i > start and arr[i] == arr[i-1]) :
+                    continue
+                temp = pref + [arr[i]]
+                generate(arr, i+1, temp, acc)
+            
+            return acc
+
+        arr.sort() # To reduce the number of recursive calls
+        ans = generate(arr, 0, [], [])
+
+        return ans
