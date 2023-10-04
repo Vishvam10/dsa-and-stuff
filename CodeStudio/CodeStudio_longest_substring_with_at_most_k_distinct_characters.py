074d0f7d8d66d4a5df779acc4653fa8c24ef9ff6
Vishvam10 <s.vishvam1025@gmail.com>
2023-10-04 22:17:36 +0530

    Added CodeStudio : create_binary_tree

---
+++ CodeStudio/CodeStudio_longest_substring_with_at_most_k_distinct_characters.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+def getLengthofLongestSubstring(k, s):
+    i, j, n = 0, 0, len(s)
+    mp = defaultdict(int)
+    ans = 0
+
+    while(i < n) :
+        ch = s[i]
+        mp[ch] += 1
+        while(len(mp) > k) :
+            ch1 = s[j]
+            mp[ch1] -= 1
+            if(mp[ch1] == 0) :
+                mp.pop(ch1)
+            j += 1
+        
+        ans = max(ans, i-j+1)
+        i += 1
+    
+    return ans
+
+
+
+# Main Code.
+t=int(input())
+
+for i in range(t):
+    k= int(input())
+    s= input().strip()
+    ans = getLengthofLongestSubstring(k, s)
+
+    print(ans)
