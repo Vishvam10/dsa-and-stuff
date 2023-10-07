bfdb4526f7c642743fd78438f6e205071e93494f
Vishvam10 <s.vishvam1025@gmail.com>
2023-10-07 18:57:29 +0530

    add: (codestudio) minimum-number-of-platforms

---
+++ CodeStudio/minimum-number-of-platforms/main.cpp
@@ -0,0 +1,21 @@
+int calculateMinPatforms(int arr[], int dep[], int n) {
+    // Write your code here.
+    sort(arr, arr + n);
+    sort(dep, dep + n);
+
+    int i = 1, j = 0, platforms = 1, ans = 1;
+
+    while(i < n && j < n) {
+        if(arr[i] < dep[j]) {
+            platforms++;
+            i++;
+        } else {
+            platforms--;
+            j++;
+        }
+        ans = max(ans, platforms);
+    }
+    
+    return ans;
+
+}
