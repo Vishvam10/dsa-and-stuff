9c64b7cb36d9a7bc07896580cd65214bb9794940
Vishvam10 <s.vishvam1025@gmail.com>
2023-10-09 21:41:37 +0530

    add: (codestudio) infix-to-postfix

---
+++ CodeStudio/infix-to-postfix/main.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+def infixToPostfix(s: str) -> str:
+    
+    prec = {'+': 1,'-': 1,'*': 2,'/': 2,'^': 3,'(': 0}
+    ans = ""
+    st = deque()
+
+    for ch in s :
+        
+        if(ch == '(') :
+            st.append(ch)
+        
+        elif(ch == ')') :
+            
+            while(st[-1] != '(') :
+                ans += st.pop()
+            
+            # Pop the (
+            st.pop()
+
+        elif(ch in prec) :
+
+            while(st and prec[st[-1]] >= prec[ch]) :
+                ans += st.pop()
+            
+            st.append(ch)
+
+        else :
+
+            ans += ch
+        
+    while(st) :
+        ans += st.pop()
+        
+    return ans
+
+
+
+
+
+
+
+
+
+
