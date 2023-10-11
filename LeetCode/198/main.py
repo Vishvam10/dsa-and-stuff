25dd8792c60f43d356ccf17fe93631e17dd108ce
Vishvam10 <s.vishvam1025@gmail.com>
2023-10-11 21:13:47 +0530

    add: (leetcode) 198

--- LeetCode/198/main.py
+++ LeetCode/198/main.py
@@ -1,40 +1,23 @@
 class Solution:
-    # OPTION 1 :
-    # Rob the ith house, then can't rob the previous house (i-1)
-    # => money = everything from (i-2)th house + money from current house
-    
-    # OPTION 2 :
-    # Don't rob the ith house, then we can rob the previous house (i-1)
-    # => money = everything from (i-1)th house
-    
-    # ANSWER = max(OPTION1, OPTION2). Continue this till i=n
-    
-    # Here, instead of starting with i, we start with (i+1) 
-    # which nicely makes the other 2 terms i and (i-1) respectively
-    
     def rob(self, arr: List[int]) -> int:
+        
+        def solve(ind) :
+            if(ind < 0) :
+                return 0
+            
+            if(mp[ind] >= 0) :
+                return mp[ind]
+
+            yes = arr[ind] + solve(ind - 2)
+            no = solve(ind - 1)
+            ans = max(yes, no)
+            
+            mp[ind] = ans
+
+            return ans
+
         n = len(arr)
-        if(n == 0) :
-            return 0
-        # ans = [None]*(n+1)
-        # ans[0] = 0
-        # ans[1] = arr[0]
-        # for i in range(1, n) :
-        #     ans[i+1] = max(ans[i-1] + arr[i], ans[i])
-        # return ans[n]
-    
-        # FURTHER OPTIMIZATION : Clearly, at a time, we need
-        # only 2 variables : ans[i] and ans[i-1]
-        
-        a, b = 0, 0
-        for i in range(n) :
-            temp = a
-            a = max(a, b+arr[i])
-            b = temp
-        return a
-    
-        
-            
-            
-        
-        
+        mp = [-1 for i in range(n+1)]
+        ans = solve(n-1)
+
+        return ans
