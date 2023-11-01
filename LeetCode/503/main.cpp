8423c342bce1e15337fb0fc99819ded00b932368
Vishvam10 <s.vishvam1025@gmail.com>
2023-11-01 13:52:54 +0530

    add: (leetcode) 503

---
+++ LeetCode/503/main.cpp
@@ -0,0 +1,33 @@
+#include <stack>
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr) {
+        
+        int n = arr.size();
+        vector<int> ans(n, -1);
+        stack<int> st;
+
+        for(int i = 2*n-1; i >= 0; i--) {
+
+            while(!st.empty() && arr[i % n] >= st.top()) {
+                st.pop();
+            }
+
+            if(i < n) {
+                if(st.empty()) {
+                    ans[i] = -1;
+                } else {
+                    ans[i] = st.top();
+                }
+            }
+
+            st.push(arr[i % n]);
+
+        }
+
+        return ans;
+
+    }
+
+};;
---
+++ LeetCode/503/main.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Solution:
+    def nextGreaterElements(self, arr: List[int]) -> List[int]:
+        
+        n = len(arr)
+        ans = [-1] * n
+        st = []
+
+        # Copy the array to take care of circular part
+        # Hence the 2n - 1 
+
+        for i in range(2 * n - 1, -1, -1):
+            while st and st[-1] <= arr[i % n]:
+                st.pop()
+
+            # We only want the nge's of first n elements
+            if i < n:
+                if st:
+                    ans[i] = st[-1]
+                    
+            st.append(arr[i % n])
+            
+        return ans
+
