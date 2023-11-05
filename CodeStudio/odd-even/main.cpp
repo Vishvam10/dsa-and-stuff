f9b5853961a0a647cd1b0dbe7a54dc0d247caf00
Vishvam10 <s.vishvam1025@gmail.com>
2023-11-05 23:58:31 +0530

    add: (codestudio) odd-even

---
+++ CodeStudio/odd-even/main.cpp
@@ -0,0 +1,8 @@
+string oddEven(int n){
+   int mask = 1;
+   bool res = n & mask;
+   if(res) {
+       return "odd";
+   } 
+   return "even";
+}
---
+++ CodeStudio/odd-even/main.py
@@ -0,0 +1,7 @@
+from typing import *
+
+def oddEven(n : int) -> str:
+    
+    mask = 1
+    res = n & mask
+    return 'odd' if res else 'even'
