b1a7c3fc39cb60f1ffbf540d4d80e5a480230fef
Vishvam10 <s.vishvam1025@gmail.com>
2023-12-03 22:49:59 +0530

    add: (leetcode) 769

---
+++ LeetCode/769/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        n = len(arr)
+        maxi, ans = float('-inf'), 0
+        for i in range(n) :
+            maxi = max(maxi, arr[i])
+            if(maxi <= i) :
+                ans += 1
+        return ans
