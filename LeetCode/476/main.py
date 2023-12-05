31595e49a8fa0ca0a8873a0fe5ae27635744c94a
Vishvam10 <s.vishvam1025@gmail.com>
2023-12-05 23:26:43 +0530

    add: (leetcode) 476

---
+++ LeetCode/476/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        y = list(str(bin(num)).replace('0b',''))
+        for i in range(len(y)):
+            if y[i] == '0':
+                y[i] = '1'
+            else:
+                y[i] = '0'   
+        return  int(''.join(y),2) 
+        
