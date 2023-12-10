3cf6ceb81c004be38bd13dfa3af261ca8c09811a
Vishvam10 <s.vishvam1025@gmail.com>
2023-12-10 21:39:30 +0530

    add: (codeforces) 580a

---
+++ CodeForces/580a.py
@@ -0,0 +1,31 @@
+def inp():
+    return(int(input()))
+
+def inlt():
+    return(list(map(int,input().split())))
+
+def insr():
+    return(input().strip())
+
+def solve() :
+    n = inp()
+    arr = inlt()
+
+    ans, c = 0, 1
+    prev = arr[0]
+
+    for i in range(1, n) :
+        if(arr[i] < prev) :
+            ans = max(ans, c)
+            c = 1
+        else :
+            c += 1
+            
+        prev = arr[i]
+    
+    ans = max(ans, c)
+
+    print(ans)
+    
+
+solve()
