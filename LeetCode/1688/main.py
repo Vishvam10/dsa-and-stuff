12cc6a4724dbffef174db60d174f616f10741721
Vishvam10 <s.vishvam1025@gmail.com>
2024-01-02 22:26:58 +0530

    add: (leetcode) 1688, 485 | (codeforces) 1914a

---
+++ LeetCode/1688/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        # ans = 0
+        # while(n > 1) :
+        #     ans += (n // 2)
+        #     if n % 2 == 0 :
+        #         n = (n // 2)  
+        #     else :
+        #         n = (n // 2) + 1
+
+        # return ans
+
+        return n - 1
+            
