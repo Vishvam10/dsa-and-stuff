22d0385bb46eec1a3269d2971513183efc3b99b1
Vishvam10 <s.vishvam1025@gmail.com>
2024-01-02 23:32:29 +0530

    add: (leetcode) 69

---
+++ LeetCode/69/main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if(x == 0 or x == 1) :
+            return x
+
+        low, high = 0, x
+
+        while(low <= high) :
+            mid = low + (high - low) // 2
+
+            if(mid == x // mid) :
+                return int(mid)
+            elif(mid < x // mid) :
+                low = mid + 1
+            else :
+                high = mid - 1
+            
+        return int(high)
+        
