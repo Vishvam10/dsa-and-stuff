24d4332eb12f5c2dcceb4963482c286fd86a17b3
Vishvam10 <s.vishvam1025@gmail.com>
2024-01-04 23:36:40 +0530

    add: (leetcode) 1390

---
+++ LeetCode/1390/main.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sumFourDivisors(self, arr: List[int]) -> int:
+        
+        def check(n) :
+            c, s = 0, 0
+            for i in range(1, int(n**0.5) + 1, 1) :
+                if(n % i == 0) :
+                    if(i * i == n) : 
+                        return 0
+                    
+                    c += 2
+                    s += (i + (n // i))
+            
+            return s if c == 4 else 0
+
+        ans = 0
+
+        for x in arr :
+            ans += check(x)
+        
+        return ans
