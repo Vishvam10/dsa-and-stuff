cb3232b2b7273501711b8582973f1fe144e355ba
Vishvam10 <s.vishvam1025@gmail.com>
2024-01-11 23:23:14 +0530

    add: (leetcode) 168

---
+++ LeetCode/168/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        int rem = 0;
+        string ans = "";
+        
+        while(n > 26) {
+            n--;
+            rem = n % 26;
+            ans = char('A' + rem) + ans;
+            n /= 26;
+        }
+
+        n--;
+        ans = char('A' + n) + ans;
+
+        return ans;
+        
+    }
+};
