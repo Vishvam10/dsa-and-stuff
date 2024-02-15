fe7e8ab95d611af7262f729bff6567980820fe21
Vishvam10 <s.vishvam1025@gmail.com>
2024-02-15 23:59:40 +0530

    add: (leetcode) 110, 112, 226

---
+++ LeetCode/110/main.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int check(TreeNode* node) {
+        if(node == nullptr) {
+            return 0;
+        }
+        
+        int left = check(node->left);
+        int right = check(node->right);
+        
+        if(left == -1 || right == -1 || abs(left - right) > 1) {
+            return -1;
+        }
+
+        int ht = 1 + max(left, right);
+        return ht;
+    }
+
+    bool isBalanced(TreeNode* root) {
+        if(root == nullptr) {
+            return true;
+        }
+
+        if(check(root) == -1)  {
+            return false;
+        }
+
+        return true;
+    }
+};
