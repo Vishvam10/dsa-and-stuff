1826146b2a7bcf435d5eb70e4de8d5512dfa4cd8
Vishvam10 <s.vishvam1025@gmail.com>
2024-02-15 23:30:59 +0530

    add: (leetcode) 104

---
+++ LeetCode/104/main.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int md(TreeNode* node, int height) {
+        if(node == nullptr) {
+            return height;
+        }
+        int left = md(node->left, height + 1);
+        int right = md(node->right, height + 1);
+        return max(left, right);
+    }
+
+    int maxDepth(TreeNode* root) {
+        return md(root, 0);
+    }
+};
