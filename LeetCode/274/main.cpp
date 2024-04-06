441cb4f0f7db23f24b029cdb3cab83e9ff9230dc
Vishvam10 <s.vishvam1025@gmail.com>
2024-04-06 19:49:34 +0530

    update: (leetcode) 274

--- LeetCode/274/main.cpp
+++ LeetCode/274/main.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int hIndex(vector<int>& arr) {
+       
         int n = arr.size();
         int ans = 0;
 
         sort(arr.begin(), arr.end());
 
         for(int i = 0; i < n; i++) {
-            int key = arr[i];
-            int val = n - i;
-            if(val >= key && key >= ans) {
-                ans = max(ans, key);
-            }
+            int key = min(arr[i], n - i);
+            ans = max(ans, key);
         }
 
         return ans;
