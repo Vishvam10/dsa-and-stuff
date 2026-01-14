38f1fc475b032f207274dab596184bb15ca6780f
Vishvam10 <s.vishvam1025@gmail.com>
2026-01-14 23:29:50 +0530

    add: (LeetCode) 543, 987

---
+++ LeetCode/543/main.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func check(node *TreeNode, ans *int) int {
+	if node == nil {
+		return 0
+	}
+	left := check(node.Left, ans)
+	right := check(node.Right, ans)
+	*ans = max(*ans, left+right)
+	return 1 + max(left, right)
+}
+
+func diameterOfBinaryTree(root *TreeNode) int {
+	ans := 0
+	check(root, &ans)
+	return ans
+}
