ea26a9fb72907f519664bf90e9a542b81b8f7d75
Vishvam10 <s.vishvam1025@gmail.com>
2026-01-24 20:01:22 +0530

    add: (LeetCode) 435, 57

---
+++ LeetCode/57/main.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    typedef vector<int> vec;
+    typedef vector<vector<int>> vec2d;
+
+    int search(vec2d& arr, int target) {
+        int low = 0, high = arr.size();
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid][0] < target)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+
+    vec2d insert(vec2d& arr, vec& newInterval) {
+        if (arr.empty()) return {newInterval};
+
+        int ind = search(arr, newInterval[0]);
+        arr.insert(arr.begin() + ind, newInterval);
+
+        // Now, just merge intervals
+        vec2d ans;
+        
+        ans.emplace_back(arr[0]);
+
+        for(int i = 1; i < arr.size(); ++i) {
+            vec& prev = ans.back();
+            vec& cur = arr[i];
+
+            if(cur[0] <= prev[1]) {
+                prev[0] = min(prev[0], cur[0]);
+                prev[1] = max(prev[1], cur[1]);
+            } else {
+                ans.emplace_back(cur);
+            }
+        }
+        return ans;
+    }
+};
