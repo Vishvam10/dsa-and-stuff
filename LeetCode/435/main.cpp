05b9febcb1046b11be7a23379861b579ad68eefd
Vishvam10 <s.vishvam1025@gmail.com>
2026-01-28 18:14:05 +0530

    modify: (LeetCode) 1020, 130, 207, 435, 45, 542, 57

--- LeetCode/435/main.cpp
+++ LeetCode/435/main.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& arr) {
-        int n = arr.size();
-        if(n == 1) return 0;
+  int eraseOverlapIntervals(vector<vector<int>> &arr) {
+    int n = arr.size();
+    if (n == 1)
+      return 0;
 
-        sort(arr.begin(), arr.end(), [](auto &a, auto &b) {
-            return a[1] < b[1];
-        });
+    sort(arr.begin(), arr.end(), [](auto &a, auto &b) { return a[1] < b[1]; });
 
-        int prevEnd = arr[0][1];
-        vector<int> cur;
-        int ans = 0;
+    int prevEnd = arr[0][1];
+    vector<int> cur;
+    int ans = 0;
 
-        for(int i = 1; i < n; ++i) {
-            cur = arr[i];
-            if(cur[0] < prevEnd) {
-                ans++;
-            } else {
-                prevEnd = cur[1];
-            }
-        }
-
-        return ans;
-        
+    for (int i = 1; i < n; ++i) {
+      cur = arr[i];
+      if (cur[0] < prevEnd) {
+        ans++;
+      } else {
+        prevEnd = cur[1];
+      }
     }
+
+    return ans;
+  }
 };
