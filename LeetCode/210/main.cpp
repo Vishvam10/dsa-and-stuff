e6c381d6dbff076ac1e984f0c512f93dd5e8226b
Vishvam10 <vishvam.sundararajan@contentstack.com>
2026-01-28 18:14:05 +0530

    modify: (LeetCode) 1020, 130, 207, 435, 45, 542, 57

--- LeetCode/210/main.cpp
+++ LeetCode/210/main.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-  bool canFinish(int n, vector<vector<int>> &arr) {
+  vector<int> findOrder(int n, vector<vector<int>> &arr) {
     vector<vector<int>> adj(n);
     vector<int> inDegrees(n);
+    vector<int> ans;
     queue<int> q;
-    int count = 0;
 
-    for (const auto &ele : arr) {
-      adj[ele[1]].push_back(ele[0]);
-      inDegrees[ele[0]]++;
+    for (const auto &x : arr) {
+      adj[x[1]].emplace_back(x[0]);
+      inDegrees[x[0]]++;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       if (inDegrees[i] == 0) {
         q.push(i);
       }
     }
 
     while (!q.empty()) {
+
       int node = q.front();
       q.pop();
-      count++;
+      ans.emplace_back(node);
 
-      for (const auto nei : adj[node]) {
+      for (const auto &nei : adj[node]) {
         inDegrees[nei]--;
         if (inDegrees[nei] == 0) {
           q.push(nei);
@@ -30,6 +31,9 @@ public:
       }
     }
 
-    return count == n;
+    if (ans.size() != n)
+      return {};
+
+    return ans;
   }
 };
--- LeetCode/542/main.cpp
+++ LeetCode/542/main.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
+  vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+    int nRows = mat.size(), nCols = mat[0].size();
+    vector<vector<int>> ans(nRows, vector<int>(nCols, 0));
+    vector<vector<bool>> vis(nRows, vector<bool>(nCols, false));
 
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int nRows = mat.size(), nCols = mat[0].size();
-        vector<vector<int>> ans(nRows, vector<int>(nCols, 0));
-        vector<vector<bool>> vis(nRows, vector<bool>(nCols, false));
+    queue<pair<pair<int, int>, int>> q;
 
-        queue<pair<pair<int, int>, int>> q;
-        
-        for(int i = 0; i < nRows; ++i) {
-            for(int j = 0; j < nCols; ++j) {
-                if(mat[i][j] == 0) {
-                    vis[i][j] = true;
-                    q.push({{i, j}, 0});
-                }
-            }   
+    for (int i = 0; i < nRows; ++i) {
+      for (int j = 0; j < nCols; ++j) {
+        if (mat[i][j] == 0) {
+          vis[i][j] = true;
+          q.push({{i, j}, 0});
         }
-
-        int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, 1, -1};
-
-        while(!q.empty()) {
-            auto [cell, dis] = q.front();
-            int x = cell.first;
-            int y = cell.second;
-
-            q.pop();
-	        ans[x][y] = dis; 
-
-            for(int i = 0; i < 4; ++i) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if(
-                    nx >= 0 && nx < nRows && ny >= 0 && ny < nCols &&
-                    !vis[nx][ny]
-                ) {
-                    vis[nx][ny] = true;
-                    q.push({{nx, ny}, dis + 1});
-                }
-            }
-        }
-
-        return ans;
-
+      }
     }
+
+    int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, 1, -1};
+
+    while (!q.empty()) {
+      auto [cell, dis] = q.front();
+      int x = cell.first;
+      int y = cell.second;
+
+      q.pop();
+      ans[x][y] = dis;
+
+      for (int i = 0; i < 4; ++i) {
+        int nx = x + dx[i], ny = y + dy[i];
+        if (nx >= 0 && nx < nRows && ny >= 0 && ny < nCols && !vis[nx][ny]) {
+          vis[nx][ny] = true;
+          q.push({{nx, ny}, dis + 1});
+        }
+      }
+    }
+
+    return ans;
+  }
 };
