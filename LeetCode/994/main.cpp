a7eda434d6cc84fcbb11b42e819ef18c44486444
Vishvam10 <s.vishvam1025@gmail.com>
2026-01-29 22:56:53 +0530

    modify: (LeetCode) 994

--- LeetCode/994/main.cpp
+++ LeetCode/994/main.cpp
@@ -1,60 +1,56 @@
 class Solution {
 public:
-  int orangesRotting(vector<vector<int>> &mat) {
+    int orangesRotting(vector<vector<int>>& mat) {
+        int nRows = mat.size(), nCols = mat[0].size();
+        int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, 1, -1};
+        int ans = 0;
+        queue<pair<int, int>> q;
 
-    int m = mat.size();
-    int n = mat[0].size();
-
-    int dr[] = {0, 0, -1, 1};
-    int dc[] = {-1, 1, 0, 0};
-
-    int steps = 0;
-
-    vector<vector<bool>> vis(m, vector<bool>(n, false));
-
-    queue<pair<pair<int, int>, int>> q;
-
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (mat[i][j] == 2) {
-          vis[i][j] = true;
-          q.push({{i, j}, 0});
+        for(int i = 0; i < nRows; ++i) {
+            for(int j = 0; j < nCols; ++j) {
+                if(mat[i][j] == 2) {
+                    q.push({i, j}); 
+                }
+            }
         }
-      }
-    }
 
-    while (!q.empty()) {
+        while(!q.empty()) {
 
-      int x = q.front().first.first;
-      int y = q.front().first.second;
-      int t = q.front().second;
+            bool updated = false;
+            int n = q.size();
 
-      steps = max(steps, t);
+            for(int i = 0; i < n; ++i) {
 
-      q.pop();
+                auto [x, y] = q.front();
+                q.pop();
 
-      vector<pair<int, int>> neighbours;
+                for(int j = 0; j < 4; j++) {
+                    int nx = x + dx[j], ny = y + dy[j];
+                    if(
+                        (nx >= 0 && nx < nRows && ny >= 0 && ny < nCols) &&
+                        mat[nx][ny] == 1
+                    ) {
+                        mat[nx][ny] = 2;
+                        q.push({nx, ny});
+                        updated = true;
+                    }
+                }
 
-      for (int i = 0; i < 4; i++) {
-        int r = x + dr[i];
-        int c = y + dc[i];
+            }
+            ans += (updated == true);
+        };
 
-        if ((r >= 0 && r < m && c >= 0 && c < n) &&
-            (!vis[r][c] && mat[r][c] == 1)) {
-          vis[r][c] = true;
-          q.push({{r, c}, t + 1});
+
+        for(int i = 0; i < nRows; ++i) {
+            for(int j = 0; j < nCols; ++j) {
+                if(mat[i][j] == 1) {
+                    return -1;
+                }
+            }
         }
-      }
-    }
 
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (!vis[i][j] && mat[i][j] == 1) {
-          return -1;
-        }
-      }
-    }
 
-    return steps;
-  }
+        return ans;
+
+    }
 };
