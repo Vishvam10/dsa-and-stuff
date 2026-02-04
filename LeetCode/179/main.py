536c5cd8d370a37e22dc4add24fdbabe27afb5e4
Vishvam10 <s.vishvam1025@gmail.com>
2026-02-05 01:11:00 +0530

    add: (LeetCode) 179

---
+++ LeetCode/179/main.py
@@ -0,0 +1,16 @@
+from functools import cmp_to_key
+
+class Solution:
+    def largestNumber(self, arr: List[int]) -> str:
+        def compare(a: str, b: str) -> int:
+            if a + b > b + a:
+                return -1
+            elif a + b < b + a:
+                return 1
+            else:
+                return 0
+
+        nums = list(map(str, arr))
+        nums.sort(key=cmp_to_key(compare))
+
+        return "0" if nums[0] == "0" else "".join(nums)
