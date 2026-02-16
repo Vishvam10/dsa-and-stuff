2b2125ed98d7a4b3afd3bbefd825bb12d5de5010
Vishvam10 <s.vishvam1025@gmail.com>
2026-02-16 20:49:03 +0530

    add: (LeetCode) 494

--- LeetCode/7/main.py
+++ LeetCode/7/main.py
@@ -1,9 +1,9 @@
 class Solution:
     def reverse(self, x: int) -> int:
         ans = str(x)[::-1]
-        if(ans[-1] == "-") :
+        if ans[-1] == "-":
             ans = "-" + ans[:-1]
         ans = int(ans)
-        if(ans < -2 ** 31 or ans > (2 ** 31) - 1) :
+        if ans < -(2**31) or ans > (2**31) - 1:
             ans = 0
         return ans
