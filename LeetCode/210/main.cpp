0073a6b82e52aab4c646579567134a69daf66889
Vishvam10 <s.vishvam1025@gmail.com>
2026-03-23 23:05:42 +0530

    add: (LeetCode) 208

--- LeetCode/210/main.cpp
+++ LeetCode/210/main.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-  vector<int> findOrder(int n, vector<vector<int>> &arr) {
-    vector<vector<int>> adj(n);
-    vector<int> inDegrees(n);
-    vector<int> ans;
-    queue<int> q;
+    vector<int> findOrder(int nCourses, vector<vector<int>>& arr) {
+        vector<vector<int>> adj(nCourses);
+        vector<int> indegrees(nCourses, 0);
+        queue<int> q;
+        vector<int> ans;
+        int cnt = 0;
 
-    for (const auto &x : arr) {
-      adj[x[1]].emplace_back(x[0]);
-      inDegrees[x[0]]++;
-    }
-
-    for (int i = 0; i < n; ++i) {
-      if (inDegrees[i] == 0) {
-        q.push(i);
-      }
-    }
-
-    while (!q.empty()) {
-
-      int node = q.front();
-      q.pop();
-      ans.emplace_back(node);
-
-      for (const auto &nei : adj[node]) {
-        inDegrees[nei]--;
-        if (inDegrees[nei] == 0) {
-          q.push(nei);
+        for(int i = 0; i < arr.size(); ++i) {
+            adj[arr[i][1]].push_back(arr[i][0]);
+            indegrees[arr[i][0]]++;
         }
-      }
+
+        for(int i = 0; i < nCourses; ++i) {
+            if(indegrees[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.emplace_back(node);
+            cnt++;
+            for(int nei : adj[node]) {
+                indegrees[nei]--;
+                if(indegrees[nei] == 0) {
+                    q.push(nei);
+                }
+            }
+        }
+        
+        return (cnt == nCourses) ? ans : vector<int>{};
     }
-
-    if (ans.size() != n)
-      return {};
-
-    return ans;
-  }
 };
