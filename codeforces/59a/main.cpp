42f770cf7e92699b0b3647187ba0a83f154548d6
Vishvam10 <s.vishvam1025@gmail.com>
2026-09-02 22:53:33 +0530

    modify: (codeforces) 59a

--- codeforces/59a/main.cpp
+++ codeforces/59a/main.cpp
@@ -1,30 +1,53 @@
 #include <algorithm>
+#include <array>
+#include <cassert>
 #include <cctype>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <deque>
+#include <functional>
+#include <iomanip>
 #include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
 #include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
 
+using namespace std;
+
+void solve() {
+    string s;
+    cin >> s;
+
+    int upper = count_if(s.begin(), s.end(), ::isupper);
+
+    if (upper > s.length() / 2) {
+        transform(s.begin(), s.end(), s.begin(), ::toupper);
+    } else {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+    }
+
+    cout << s << '\n';
+}
 int main() {
-
-	std::string s;
-	std::cin >> s;
-
-	int a = 0, b = 0;
-
-	for (int i = 0; i < s.length(); i++) {
-		if (std::isupper(s[i])) {
-			a++;
-		} else {
-			b++;
-		}
-	}
-
-	if (a > b) {
-		std::transform(s.begin(), s.end(), s.begin(), ::toupper);
-		std::cout << s << std::endl;
-	} else {
-		std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-		std::cout << s << std::endl;
-	}
-
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
 }
